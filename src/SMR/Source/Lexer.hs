defab8d3e30f337238e3ad84fa4db2e777850790
Ben Lippmeier <benl@ouroborus.net>
2017-10-02 15:48:52 +1100

    Add lexer

---
+++ src/SMR/Source/Lexer.hs
@@ -0,0 +1,198 @@
+
+module SMR.Source.Lexer
+        ( lexTokens
+        , Located (..)
+        , Location(..))
+where
+import SMR.Source.Token
+import Data.Text                (Text)
+import qualified Data.Text      as Text
+import qualified Data.Char      as Char
+
+
+-- Lexer ----------------------------------------------------------------------
+-- | Lex a sequence of tokens.
+lexTokens :: Location -> [Char] -> ([Located Token], Location, [Char])
+lexTokens lStart0 cs0
+ = case skipSpace lStart0 cs0 of
+    (lStart, [])
+     -> ( LL lStart lStart KEnd : []
+        , lStart, [])
+
+    (lStart, cs)
+     -> case lexToken lStart cs of
+         Nothing
+          -> ([], lStart, cs)
+
+         Just (k, cs')
+          |  (ks, lStart', cs'') <- lexTokens (endOfLocated k) cs'
+          -> (k : ks, lStart', cs'')
+
+
+-- | Lex a single token.
+lexToken :: Location -> [Char] -> Maybe (Located Token, [Char])
+lexToken lStart xx
+ = case xx of
+    []
+     -> Nothing
+
+    c : cs
+        -- Punctuation.
+        |  isCharPunc c
+        -> let  lEnd = incCharOfLocation 1 lStart
+                tok  = KPunc c
+           in   Just (LL lStart lEnd tok, cs)
+
+        -- Variable name.
+        |  Just (space, xx')         <- takeSpace c cs
+        ,  Just (name, lEnd, csRest) <- lexName   (incCharOfLocation 1 lStart) xx'
+        -> let  tok      = KName space name
+           in   Just (LL lStart lEnd tok, csRest)
+
+        --  Natural number.
+        |  Char.isDigit c
+        ,  Just (nat, lEnd, csRest)  <- lexNat lStart (c : cs)
+        -> let  tok      = KNat nat
+           in   Just (LL lStart lEnd tok, csRest)
+
+        |  otherwise
+        -> Nothing
+
+
+-- | Lex a variable name.
+lexName :: Location -> [Char] -> Maybe (Text, Location, [Char])
+lexName lStart xx
+ = go lStart [] xx
+ where
+        go lStart' acc []
+         = let  name    = Text.pack $ reverse acc
+           in   Just (name, lStart', [])
+
+        go lStart' acc (c : cs)
+         | isNameBodyChar c
+         =      go (incCharOfLocation 1 lStart') (c : acc) cs
+
+         | otherwise
+         = let  name    = Text.pack $ reverse acc
+           in   Just (name, lStart', c : cs)
+
+
+-- | Lex a natural number.
+lexNat  :: Location -> [Char] -> Maybe (Int, Location, [Char])
+lexNat lStart xx
+ = go lStart [] xx
+ where
+        go lStart' acc []
+         | all Char.isDigit acc
+         , nat <- read $ reverse acc
+         = Just (nat, lStart', [])
+
+        go lStart' acc (c : cs)
+         | Char.isDigit c
+         = go (incCharOfLocation 1 lStart') (c : acc) cs
+
+         | all Char.isDigit acc
+         , nat <- read $ reverse acc
+         = Just (nat, lStart', c : cs)
+
+        go _ _ _
+         = Nothing
+
+
+
+-- Whitespace -----------------------------------------------------------------
+skipSpace :: Location -> [Char] -> (Location, [Char])
+skipSpace lStart xx
+ = case xx of
+    []  -> (lStart, xx)
+
+    c : cs
+        -- Skip whitespace.
+        | c == ' '  -> skipSpace (incCharOfLocation 1 lStart) cs
+        | c == '\n' -> skipSpace (incLineOfLocation 1 lStart) cs
+        | c == '\t' -> skipSpace (incCharOfLocation 8 lStart) cs
+
+        -- Skip comments
+        |  c  == '-'
+        ,  c2 : cs2 <- cs
+        ,  c2 == '-'
+        -> skipSpace lStart $ dropWhile (\x -> x /= '\n') cs2
+
+        | otherwise -> (lStart, xx)
+
+
+-- | Take the namespace qualifier from the front of a name.
+takeSpace :: Char -> [Char] -> Maybe (Space, [Char])
+takeSpace c cs
+ | Char.isLower c = Just (SVar, c : cs)
+ | c == '@'       = Just (SMac, cs)
+ | c == '%'       = Just (SSym, cs)
+
+ | c  == '#'
+ , c' : cs' <- cs
+ , c' == '#'
+ = Just (SKey, cs')
+
+ | c == '#'     = Just (SPrm, cs)
+ | otherwise    = Nothing
+
+
+-- Character Classes ----------------------------------------------------------
+-- | Check if this character can appear in the body of a name.
+isNameBodyChar :: Char -> Bool
+isNameBodyChar c
+ =  Char.isLower c
+ || Char.isUpper c
+ || Char.isDigit c
+ || (c == '-' || c == '\'' || c == '_')
+
+
+-- | Check if this is a punctuation character.
+isCharPunc :: Char -> Bool
+isCharPunc c
+ | c == '('     = True
+ | c == ')'     = True
+ | c == '{'     = True
+ | c == '}'     = True
+ | c == '['     = True
+ | c == ']'     = True
+ | c == '<'     = True
+ | c == '>'     = True
+ | c == '^'     = True
+ | c == ','     = True
+ | c == ':'     = True
+ | c == '\\'    = True
+ | c == '.'     = True
+ | c == ';'     = True
+ | c == '='     = True
+ | c == '$'     = True
+ | c == '!'     = True
+ | c == '~'     = True
+ | otherwise    = False
+
+
+-- Located -------------------------------------------------------------------
+-- | Location in a source file.
+data Location
+        = L  Int Int
+        deriving Show
+
+data Located a
+        = LL Location Location a
+        deriving Show
+
+
+-- | Take the endpoint of a located thing.
+endOfLocated :: Located a -> Location
+endOfLocated (LL _ end _) = end
+
+
+-- | Increment the character position of a located thing.
+incCharOfLocation :: Int -> Location -> Location
+incCharOfLocation n (L l c) = L l (c + n)
+
+
+-- | Increment the line position of a located thing.
+incLineOfLocation :: Int -> Location -> Location
+incLineOfLocation n (L l c) = L (l + n) 1
+
---
+++ src/SMR/Source/Token.hs
@@ -0,0 +1,64 @@
+
+module SMR.Source.Token
+where
+import Data.Text (Text)
+
+
+-- | Tokens for for the source language.
+data Token
+        = KEnd                  -- ^ End of input.
+        | KPunc Char            -- ^ Punctuation character.
+        | KName Space Text      -- ^ A scoped name.
+        | KNat  Int             -- ^ A natural number.
+        deriving (Show, Eq)
+
+
+-- | Name space of a name.
+data Space
+        = SVar                  -- ^ Local variable.
+        | SMac                  -- ^ Macro name.
+        | SSym                  -- ^ Symbol name.
+        | SPrm                  -- ^ Primitive name.
+        | SKey                  -- ^ Keyword (super primitive)
+        deriving (Show, Eq)
+
+
+-- | Check if a token is equal to the give none.
+isToken :: Token -> Token -> Bool
+isToken k1 k2 = k1 == k2
+
+
+-- | Check is token is punctuation using the given character.
+isKPunc :: Char -> Token -> Bool
+isKPunc c k
+ = case k of
+        KPunc c' -> c == c'
+        _        -> False
+
+
+-- | Take the name from a token, if any.
+takeNameOfToken :: Space -> Token -> Maybe Text
+takeNameOfToken ss1 kk
+ = case kk of
+        KName ss2 n
+         | ss1 == ss2   -> Just n
+         | otherwise    -> Nothing
+        _               -> Nothing
+
+
+-- | Take the name from a token, if any.
+takeAnyNameOfToken :: Token -> Maybe (Space, Text)
+takeAnyNameOfToken kk
+ = case kk of
+        KName ss2 n     -> Just (ss2, n)
+        _               -> Nothing
+
+
+-- | Take the natural number from a token, if any.
+takeNatOfToken :: Token -> Maybe Int
+takeNatOfToken kk
+ = case kk of
+        KNat n          -> Just n
+        _               -> Nothing
+
+
